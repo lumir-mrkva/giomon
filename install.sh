ed94c7aff601e8f7addf3e60d0f56bc6a14cb27e
Lumir Mrkva <lumir.mrkva@topmonks.com>
2019-12-04 11:41:28 +0100

    install service

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
 pip3 install -r ./requirements.txt
 cp monitor.py /usr/bin
-ln -s monitor.service /etc/systemd/system/monitor.service
+cp monitor.service /lib/systemd/system
+ln -s /lib/systemd/system/monitor.service /etc/systemd/system/monitor.service
+systemctl enable monitor
+systemctl start monitor
