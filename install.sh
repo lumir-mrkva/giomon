d4fa85b18cda95832af5a219c7ee2bd3590ec5aa
Lumir Mrkva <lumir.mrkva@topmonks.com>
2019-12-26 16:54:21 +0100

    fixed install script

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 pip3 install -r ./requirements.txt
-cp monitor.py /usr/bin
+cp *.py /usr/bin
 cp monitor.service /lib/systemd/system
 ln -s /lib/systemd/system/monitor.service /etc/systemd/system/monitor.service
 systemctl enable monitor
